08a9e293906ea00fa89d11ad6af8ff3f1a73c741
LalithKumar P <lalithkumar.p@localadmins-MacBook-Pro.local>
2016-09-09 21:41:29 +0530

    changes for mongodb data insertion

--- src/com/jarvis/util/Mongo.java
+++ src/com/jarvis/util/Mongo.java
@@ -26,9 +26,45 @@ class Mongo {
         coll.insert(data);
     }
     
-   public JSONObject getDataFromDb(String dbName, String user_id) {
+   public ArrayList getDataFromDb(String dbName, String user_id) {
 	   DB db = this.conn.getDB(dbName);
-	   return new JSONObject();
-	   
+	   DBCollection coll = db.getCollection("user_activity");
+	   BasicDBObject query = new BasicDBObject();
+	   query.put("user_id", user_id);
+	   DBCursor cursor = coll.find(query);
+	   ArrayList<Object> userData = new ArrayList<Object>();
+	   while (cursor.hasNext()) {
+		   Object doc = cursor.next();
+		   userData.add(doc);
+	       System.out.println(doc);
+	   }
+	   return userData;
    }
+   
+   public void removeDataFromDb(String dbName, String user_id) {
+	   DB db = this.conn.getDB(dbName);
+	   DBCollection coll = db.getCollection("user_activity");
+	   BasicDBObject query = new BasicDBObject();
+	   query.put("user_id", user_id);
+	   DBCursor cursor = coll.find(query);
+	   ArrayList<Object> userData = new ArrayList<Object>();
+	   while (cursor.hasNext()) {
+		   DBObject doc = cursor.next();
+		  System.out.println(doc);
+		  coll.remove(doc);
+	   }
+   }
+   
+   public static void main(String[] args) throws Exception {
+
+	    Mongo mongo = new Mongo();
+	    String userId = "123";
+	    String dbName = "user_data";
+	    HashMap<String, Object> userData = new HashMap<String, Object>();
+	    userData.put("a", "1");
+	    userData.put("b", "2");
+	    userData.put("c", "3");
+	    mongo.writeDataToDb(dbName, userId, userData);
+
+	}
 }
