261d5b2da8ec4ac7ddf38fd212b612a2839877b0
LalithKumar P <lalithkumar.p@localadmins-MacBook-Pro.local>
2016-09-09 17:24:56 +0530

    changes for mongo

--- src/com/jarvis/util/Mongo.java
+++ src/com/jarvis/util/Mongo.java
@@ -1,20 +1,22 @@
+package com.jarvis.util;
 import com.mongodb.*;
 import com.mongodb.util.JSON;
 import com.mongodb.MongoClient;
 import java.util.HashMap; 
+import org.json.*;
+import java.util.*;
 
 class Mongo {
     static MongoClient conn = null;
 
-    public static MongoClient getConnection() {
+    public Mongo() {
         if(conn == null) {
-            conn = new MongoClient("localhost", "27017");
+            conn = new MongoClient("localhost", 27017);
         }
-        return conn;
     }
 
-    public static void writeDataToDb(String dbName, String user_id, JSONObject jsonData) {
-        DB db = this.getConnection().getDB(dbName);
+    public  void writeDataToDb(String dbName, String user_id, HashMap<String, Object> jsonData) {
+        DB db = this.conn.getDB(dbName);
         DBCollection coll = db.getCollection("user_activity");
         DBObject data = new BasicDBObject();
         data.put("user_id", user_id);
@@ -23,4 +25,10 @@ class Mongo {
         }
         coll.insert(data);
     }
+    
+   public JSONObject getDataFromDb(String dbName, String user_id) {
+	   DB db = this.conn.getDB(dbName);
+	   return new JSONObject();
+	   
+   }
 }
